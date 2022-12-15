ea8157ae5037d0d2dfbc13acb6724c49bb0bad88
lichunghan <2608367047@qq.com>
2022-12-15 13:45:52 +0800

    test

---
+++ aaa.py
@@ -0,0 +1,26 @@
+# -*- coding:UTF-8 -*-
+# file_name     :aaa.py
+# author        :lichuanghan
+# create_date   :2022/12/15
+import os
+
+"""
+#
+#
+#
+"""
+def find_path(dir, name):
+    filePaths = []
+    for root, dirs, names in os.walk(dir):
+        if(name in names):
+            path = root + '/' + name
+            filePaths.append(path)
+    if(len(filePaths)==1):
+        return filePaths[0]
+    else:
+        return filePaths
+
+
+if __name__ == '__main__':
+    a = find_path(".", "aaa.py")
+    print(a)
